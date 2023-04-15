b3aec3167f7088f089592fa55fcf7724047b20cc
bleedinglight <halpin2003@gmail.com>
2023-04-15 16:30:32 +0100

    added ai state machine, cian ai completed

--- AI Project/Assets/AIControllerCian.cs
+++ AI Project/Assets/AIControllerCian.cs
@@ -1,17 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class AIControllerCian : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
     public float timer, wanderTime;
+    public Transform otherPlayer;
+
+    public enum State
+    {
+        FindCharacter,
+        Wander,
+        Idle
+    }
+
+    private State _currentState;
 
     // Start is called before the first frame update
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _currentState = State.FindCharacter;
     }
 
     // Update is called once per frame
@@ -22,13 +35,39 @@ public class AIControllerCian : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                SetAITargetLocation(hit.point);
+                //SetAITargetLocation(hit.point);
             }
             
         }
 
-        timer += Time.deltaTime;
-        Wander();
+        
+
+        switch (_currentState)
+        {
+            case State.FindCharacter:
+                
+                SetAITargetLocation(otherPlayer.position);
+
+                if (_navMeshAgent.remainingDistance < 1.0f && _navMeshAgent.remainingDistance > 0.5f)
+                {
+                    _currentState = State.Wander;
+                }
+
+                break;
+            
+            case State.Wander:
+                
+                timer += Time.deltaTime;
+                Wander();
+
+                break;
+            
+            case State.Idle:
+                
+                SetAITargetLocation(transform.position);
+
+                break;
+        }
     }
 
     private void SetAITargetLocation(Vector3 targetLocation)
@@ -47,6 +86,11 @@ public class AIControllerCian : MonoBehaviour
             timer = 0;
         }
         
+    }
+
+    public void SetState(string newState)
+    {
+        _currentState = (State) Enum.Parse(typeof(State), newState);
     }
 }
 
